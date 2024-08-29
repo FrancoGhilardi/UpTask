71d337e6a352ce4ee89567748fea0058116be1ea
franco <franco,ghilardi1@gmail.com>
2024-08-29 14:48:29 -0300

    Se termina flujo completo para creacion de usuario

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -8,7 +8,7 @@ interface IEmail {
 
 export class AuthEmail {
   static sendConfirmationEmail = async (user: IEmail) => {
-    const info = await transporter.sendMail({
+    await transporter.sendMail({
       from: "UpTask <admin@uptask.com>",
       to: user.email,
       subject: "UpTask - Confirmar cuenta",
@@ -17,19 +17,31 @@ export class AuthEmail {
                 <h2 style="color: #333;">Hola, ${user.name}</h2>
                 <p style="font-size: 16px;">Has creado tu cuenta en <strong>UpTask</strong>. ¡Ya casi está todo listo! Solo debes confirmar tu cuenta.</p>
                 <p style="font-size: 16px;">Visita el siguiente enlace para confirmar tu cuenta:</p>
-                <a href="" style="display: inline-block; padding: 10px 20px; margin: 10px 0; background-color: #28a745; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Confirmar cuenta</a>
+                <a href="${process.env.FRONTEND_URL}/auth/confirm-account" style="display: inline-block; padding: 10px 20px; margin: 10px 0; background-color: #28a745; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Confirmar cuenta</a>
                 <p style="font-size: 16px;">Además, ingresa el siguiente código:</p>
                 <p style="font-size: 20px; font-weight: bold; background-color: #e9ecef; padding: 10px; border-radius: 5px; text-align: center; letter-spacing: 1.5px;">${user.token}</p>
                 <p style="font-size: 14px; color: #666;">Este token expira en 10 minutos.</p>
             </div>
       `,
-      //   html: `<p>Hola: ${user.name}, has creado tu cuenta en UpTask, ya casi esta todo listo, solo debes confirmar tu cuenta</p>
-      //   <p>Visita el siguiente enlace:</p>
-      //   <a href="">Confirmar cuenta</a>
-      //   <p>E ingresa el código: <b>${user.token}</b></p>
-      //   <p>Este token expira en 10 minutos</p>
-      //   `,
     });
-    // return info
+  };
+
+  static sendPasswordResetToken = async (user: IEmail) => {
+    await transporter.sendMail({
+      from: "UpTask <admin@uptask.com>",
+      to: user.email,
+      subject: "UpTask - Reestablece tu contraseña",
+      text: "UpTask - Reestablece tu contraseña",
+      html: `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f4f4f4; border-radius: 8px; color: #333;">
+                <h2 style="color: #333;">Hola, ${user.name}</h2>
+                <p style="font-size: 16px;">Has solicitado restablecer tu contraseña.</p>
+                <p style="font-size: 16px;">Visita el siguiente enlace para restablecer tu contraseña:</p>
+                <a href="${process.env.FRONTEND_URL}/auth/new-password" style="display: inline-block; padding: 10px 20px; margin: 10px 0; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Restablecer contraseña</a>
+                <p style="font-size: 16px;">Además, ingresa el siguiente código:</p>
+                <p style="font-size: 20px; font-weight: bold; background-color: #e9ecef; padding: 10px; border-radius: 5px; text-align: center; letter-spacing: 1.5px;">${user.token}</p>
+                <p style="font-size: 14px; color: #666;">Este token expira en 10 minutos.</p>
+              </div>
+      `,
+    });
   };
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { handleImputError } from "../middleware/validations";
 
 const router = Router();
@@ -11,11 +11,12 @@ router.post(
   body("password")
     .isLength({ min: 8 })
     .withMessage("¡Contraseña demasiado corta, minimo 8 caracteres!"),
-  body("password-confirmation").custom((value, { req }) => {
+  body("password_confirmation").custom((value, { req }) => {
     if (value !== req.body.password) {
       throw new Error("¡Las contraseñas no coinciden!");
     }
     return true;
+    1;
   }),
   body("email").isEmail().withMessage("¡El email no es valido!"),
   handleImputError,
@@ -35,6 +36,43 @@ router.post(
   body("password").notEmpty().withMessage("¡La contraseña no puede ir vacia!"),
   handleImputError,
   AuthController.login
+);
+
+router.post(
+  "/request-code",
+  body("email").isEmail().withMessage("¡El email no es valido!"),
+  handleImputError,
+  AuthController.requestConfirmationCode
+);
+
+router.post(
+  "/forgot-password",
+  body("email").isEmail().withMessage("¡El email no es valido!"),
+  handleImputError,
+  AuthController.forgotPassword
+);
+
+router.post(
+  "/validate-token",
+  body("token").notEmpty().withMessage("¡El Token no puede ir vacio!"),
+  handleImputError,
+  AuthController.validateToken
+);
+
+router.post(
+  "/update-password/:token",
+  param("token").isNumeric().withMessage("¡Token no valido!"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("¡Contraseña demasiado corta, minimo 8 caracteres!"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("¡Las contraseñas no coinciden!");
+    }
+    return true;
+  }),
+  handleImputError,
+  AuthController.updatePasswordWithToken
 );
 
 export default router;
