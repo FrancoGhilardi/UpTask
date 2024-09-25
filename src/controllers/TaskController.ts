d323b217a06be95b7ab8361e9910290ee87bbaa5
franco <franco,ghilardi1@gmail.com>
2024-09-24 21:09:22 -0300

    Se termina con CRUD de tareas

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -27,12 +27,11 @@ export class TaskController {
 
   static getTaskById = async (req: Request, res: Response) => {
     try {
-      if (req.task.project.toString() !== req.project.id) {
-        const error = new Error("¡Tarea no valida!");
-        return res.status(400).json({ error: error.message });
-      }
-
-      res.json(req.task);
+      const task = await Task.findById(req.task.id).populate({
+        path: "completedBy.user",
+        select: "id name email",
+      });
+      res.json(task);
     } catch (error) {
       res.status(500).json({ error: "¡Hubo un error!" });
     }
@@ -66,6 +65,8 @@ export class TaskController {
     try {
       const { status } = req.body;
       req.task.status = status;
+      const data = { user: req.user.id, status };
+      req.task.completedBy.push(data);
       await req.task.save();
       res.send("¡Estado de la tarea editado correctamente!");
     } catch (error) {
