6cf83fc2abb86a8106b55ad2b2266e9ef8117829
franco <franco,ghilardi1@gmail.com>
2024-10-03 19:51:18 -0300

    Se crean metodos para mdoficiar el perfil y contraseña

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -203,4 +203,38 @@ export class AuthController {
   static user = async (req: Request, res: Response) => {
     return res.json(req.user);
   };
+
+  static updateProfile = async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists && userExists.id.toString() !== req.user.id.toString()) {
+      const error = new Error("¡Ese email ya esta registrado!");
+      res.status(409).json({ error: error.message });
+    }
+    req.user.name = name;
+    req.user.email = email;
+    try {
+      await req.user.save();
+      res.send("¡Perfil actualizado correctamente!");
+    } catch (error) {
+      res.status(500).json({ error: "¡Hubo un error!" });
+    }
+  };
+
+  static updateCurrentUserPassword = async (req: Request, res: Response) => {
+    const { current_password, password } = req.body;
+    const user = await User.findById(req.user.id);
+    const isPasswordCorrect = await checkPassword(current_password, password);
+    if (!isPasswordCorrect) {
+      const error = new Error("¡La contraseña actual es incorrecta!");
+      res.status(401).json({ error: error.message });
+    }
+    try {
+      user.password = await hashPassword(password);
+      await user.save();
+      res.send("¡Contraseña actualizada correctamete!");
+    } catch (error) {
+      res.status(500).json({ error: "¡Hubo un error!" });
+    }
+  };
 }
